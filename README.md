224f2ef86a854d9e5d926df9308996f613d2a8dc
StroverIT <xcreaw1@abv.bg>
2024-01-30 22:01:41 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,19 @@
+Node version -> **v18.19.0**
+
+Multi to one line code for cookies array -> https://codebeautify.org/multiline-to-single-line
+
+
+### **How to start:**
+
+* Before running the script take cookies from https://appcenter.ms/apps . You can use Storage Ace extension for chrome and click on button copy
+After that go to https://codebeautify.org/multiline-to-single-line and make it one line
+Then paste the cookies in .env
+
+1. node index.js
+2. Write the branch you want to create builds
+
+* Note
+
+This is oppening multiple browsers and creating builds for Android and iOS dev and test
+If you want to create build for specific { OS }, go to envToCreate in index.js and choose the OS.
+If is true, then gonna create builds
---
+++ index.js
@@ -0,0 +1,112 @@
+require('dotenv').config()
+
+const puppeteer = require('puppeteer');
+
+const readline = require('readline');
+
+const OS = ["Android", "iOS"]
+const ENV = ["Dev", "Test"]
+
+const androidDevString = `${OS[0]}-${ENV[0]}`
+const androidTestString = `${OS[0]}-${ENV[1]}`
+const iosDevString = `${OS[1]}-${ENV[0]}`
+const iosTestString = `${OS[1]}-${ENV[1]}`
+
+const envToCreate = {
+  androidDev: true,
+  androidTest: true,
+  iosDev: true,
+  iosTest: true
+}
+
+const cookiesArray = JSON.parse(process.env.COOKIES_ARRAY) || []
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Branch: ", async function(branch) {
+
+  const androidDev = createAppCenterUrl(androidDevString, branch)
+  const androidTest = createAppCenterUrl(androidTestString, branch)
+  const iosDev = createAppCenterUrl(iosDevString, branch)
+  const iosTest = createAppCenterUrl(iosTestString, branch)
+
+    if(envToCreate.androidDev){
+      await createBuildForBranch(androidDev)
+    }
+    if(envToCreate.androidTest){
+      await createBuildForBranch(androidTest)
+    }
+    if(envToCreate.iosDev){
+      await createBuildForBranch(iosDev)
+    }
+    if(envToCreate.iosTest){
+      await createBuildForBranch(iosTest)
+    }
+      rl.close();
+});
+
+function createAppCenterUrl (OS_ENV, url){
+  const branchToArray = url.split("/")
+
+  return {
+    url: `${APPCENTER_URL_1}${OS_ENV}${APPCENTER_URL_2}${branchToArray[0]}%2F${branchToArray[1]}`,
+    OS_ENV
+  }
+}
+
+async function createBuildForBranch(appCenterData){
+
+  const {url, OS_ENV} = appCenterData
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const timeout = 999999999;
+  page.setDefaultTimeout(timeout);
+
+  
+    try {
+      console.log(`Starting build for: ${OS_ENV}... ` )
+      await page.setCookie(...cookiesArray);
+      await page.cookies(url); 
+  
+      await page.goto(url);
+      // _3ZPOfyk4x _73O7sIPMx
+      // Loading spinner
+      await page.waitForSelector('._3ZPOfyk4x', { hidden: true });
+  
+      //If is not cloned, clone it
+        try{
+            await page.click('button[data-test-class="button trigger"]');
+            await page.click('div[data-test-class="dropdown-list-item"]');
+            await page.click('input[type="radio"][value="develop"]');
+  
+            const [button] = await page.$x("//button[contains(., 'Clone')]");
+              if (button) {
+                  await button.click();
+              }
+        }catch(e){
+          console.log("error branch:", e)
+        }
+      // Wait for build button
+      await page.waitForSelector('button[data-test-id="build-now-button"]');
+      await page.click('button[data-test-id="build-now-button"]');
+
+      console.log(`Build for ${OS_ENV} has finished`)
+    } catch (err) {
+      console.error("error:", err);
+    } finally {
+      
+      await browser.close();
+  
+    }
+  
+};
+
+
+
+
